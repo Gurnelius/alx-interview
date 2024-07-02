a3e7bd011cdd093101167c13ebcdbfaf50b356a5
Gurnelius Terer <terergurnelius@gmail.com>
2024-07-02 11:55:41 +0300

    clean code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 """
 Module for the function
 canUnlockAll
@@ -30,8 +29,3 @@ def canUnlockAll(boxes):
                 keys.append(key)  # Add its keys to the list
 
     return all(unlocked)  # Check if all boxes are unlocked
-
-
-if __name__ == "__main__":
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Output: True
