3f87b019777310c2c9cc883456786b002030371f
Gurnelius Terer <terergurnelius@gmail.com>
2024-07-10 13:58:25 +0300

    Initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Given a number n, write a method that
+calculates the fewest number of operations needed to
+result in exactly n H characters in the file.
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All
+=> Paste => HHHHHH => Paste => HHHHHHHHH
+'''
+
+
+def minOperations(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 2
+    if n == 2:
+        return 2
+
+    if n % 2 == 0:
+        return 2 + minOperations(n/2)
+    if n % 3 == 0:
+        return 2 + minOperations(n-3)
