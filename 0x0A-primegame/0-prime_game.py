25f523c09df672e615386a050da016a0abac8356
Gurnelius Terer <terergurnelius@gmail.com>
2024-09-03 09:51:35 +0300

    initial commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,62 +9,61 @@ They play x rounds of the game, where n may be different for each round.
 Assuming Maria always goes first and both players play optimally,
 determine who the winner of each game is.
 '''
-MARIA = 0
-BEN = 1
 
 
-def is_prime(n):
-    '''
-    Check if number is a prime number.
-    '''
-    # Check if n is less than 2, which are not prime
-    if n <= 1:
-        return False
-    # 2 and 3 are prime numbers
-    if n <= 3:
-        return True
-    # Eliminate numbers divisible by 2 or 3
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    # Check divisors from 5 to the square root of n
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def is_multiple(n, x):
-    '''
-    This function checks if n is a multiple of x.
-    '''
-    return n % x == 0
+def sieve(n):
+    """
+    Return a list of prime numbers up to n using the
+    Sieve of Eratosthenes.
+    """
+    is_prime = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if is_prime[p]]
 
 
 def isWinner(x, nums):
-    '''
-    This function determines the winner of the game.
-    '''
-    player = BEN
+    """Determine who won the most rounds."""
+    if x <= 0 or not nums:
+        return None
 
-    primes = []
-    for i in range(x):
-        primes.append([j for j in range(1, nums[i]+1) if is_prime(j)])
-    for prime in primes:
-        if len(prime):
-            num = prime.pop(0)
+    # Maximum number in nums
+    max_n = max(nums)
 
-            for i in range(len(prime)):
-                if is_multiple(num, prime[i]):
-                    prime.pop(i)
-                player = (player + 1) % 2
-    if player == MARIA:
-        return 'Maria'
+    # Get list of primes up to max_n using sieve
+    primes = sieve(max_n)
+
+    # Track the number of wins
+    maria_wins = 0
+    ben_wins = 0
+
+    # Simulate each round
+    for n in nums:
+        if n == 1:
+            ben_wins += 1
+            continue
+
+        prime_count = 0
+        for p in primes:
+            if p > n:
+                break
+            # How many primes <= n
+            prime_count += 1
+
+        # If the count of primes up to n is odd, Maria wins, else Ben wins
+        if prime_count % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
-        return 'Ben'
-
-
-if __name__ == "__main__":
-    winner = isWinner(5, [2, 5, 1, 4, 3])
-    print(winner)
+        return None
